289bbe52bfdf5d09eb1514d752c243ae1398a55c
ANKIT KASHYAP <27ankitkashyap@gmail.com>
2026-02-25 16:45:49 +0530

    fix1 github pages deploy

---
+++ tests/Admin_as_admin/mycase.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://hiqa2.pooraa.net/');
+  await page.getByRole('textbox', { name: 'Email ID' }).click();
+  await page.getByRole('textbox', { name: 'Email ID' }).fill('ankit.kashyap@mail.busy.in');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123456');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'menu' }).click();
+  await page.getByRole('link', { name: 'perm_identity Partner' }).click();
+  await page.getByRole('button', { name: 'Access Account' }).nth(3).click();
+  await page.getByRole('link', { name: 'View Wallet' }).click();
+  await expect(page.locator('form')).toContainText('3M Overseas');
+});
