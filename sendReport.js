f14fe571609f6ec29512262ba147b8fc75018c53
ANKIT KASHYAP <27ankitkashyap@gmail.com>
2026-02-25 18:07:25 +0530

    fix2 github pages deploy

--- sendReport.js
+++ sendReport.js
@@ -18,22 +18,25 @@ async function sendEmail() {
     from: "ankit.kashyap@mail.busy.in",
     to: "27ankitkashyap@gmail.com",
     subject: "Playwright Automation Execution Report",
-    html: `
-      <h3>Automation Execution Completed</h3>
-      <p>Please check the report using below link:</p>
-      <a href="https://ankit-kashyap.github.io/Hi_busy_playwright-automation-reports/">
-            style="display:inline-block;
-              padding:10px 20px;
-              background-color:#0078D7;
-              color:white;
-              text-decoration:none;
-              border-radius:5px;">
-       View Automation Report
-      </a>
-      <br><br>
-      Regards,<br>
-      Ankit
-    `
+   html: `
+  <h3>Automation Execution Completed</h3>
+  <p>Please check the report using below link:</p>
+
+  <a href="https://ankit-kashyap.github.io/Hi_busy_playwright-automation-reports/"
+     style="display:inline-block;
+            padding:10px 20px;
+            background-color:#0078D7;
+            color:#ffffff;
+            text-decoration:none;
+            border-radius:5px;
+            font-weight:bold;">
+     View Automation Report
+  </a>
+
+  <br><br>
+  Regards,<br>
+  Ankit
+`
   };
 
   await transporter.sendMail(mailOptions);
